c96db494d780f0564217f22ff176b8451a4f5749
dudenesspa <dudenesspa@gmail.com>
2022-07-03 16:58:26 -0400

    Initial commit

---
+++ Assets/Scripts/CheckUnlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckUnlock : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject key;
+    public GameObject door = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        door.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Key"))
+        {
+            anim.SetBool("isOpen", true);
+            key.SetActive(false);
+
+        }
+    }
+}
